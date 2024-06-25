dcbaf89755af89c1bae7fb8177741eaecf11ffc3
Khayti1102 <115869656+Khayti1102@users.noreply.github.com>
2024-06-25 22:30:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function changeColor(color) {
+    const paragraph = document.getElementById('paragraph');
+    paragraph.style.color = color;
+}
+
+function changeSize(size) {
+    const paragraph = document.getElementById('paragraph');
+    paragraph.style.fontSize = size + 'px';
+}
+
+function changeFont(font) {
+    const paragraph = document.getElementById('paragraph');
+    paragraph.style.fontFamily = font;
+}
